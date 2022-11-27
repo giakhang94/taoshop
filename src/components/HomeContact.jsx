8e26f8291998df337a7315973e7cfa392d48d32e
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2022-11-27 13:27:07 +0700

    change align for text in home contact

--- src/components/HomeContact.jsx
+++ src/components/HomeContact.jsx
@@ -2,10 +2,10 @@ function HomeContact() {
     return (
         <div className="home-contact flex laptop:flex-row tablet:flex-col mobile:flex-col smallmobile:flex-col justify-around py-10 items-center">
             <div className="content  laptop:w-2/4 tablet:full mobile:w-full smallmobile:w-full tablet:px-5 mobile:px-3 smallmobile:px-3">
-                <h2 className="text-2xl mobile:text-center smallmobile:tex-center tablet:text-center font-semibold tracking-[2px] text-[#102A42] laptop:mb-5 tablet:mb-3 mobile:mb-2 smallmobile:mb-2">
+                <h2 className="text-2xl laptop:text-left mobile:text-center smallmobile:tex-center tablet:text-center font-semibold tracking-[2px] text-[#102A42] laptop:mb-5 tablet:mb-3 mobile:mb-2 smallmobile:mb-2">
                     Join our newsletter and get 20% off
                 </h2>
-                <p className="leading-8 laptop:pr-[20%] text-justify text-[#102A42] mobile:text-center smallmobile:tex-center tablet:text-center">
+                <p className="leading-8 laptop:pr-[20%] laptop:text-justify text-[#102A42] mobile:text-center smallmobile:tex-center tablet:text-center">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint unde quaerat ratione soluta
                     veniam provident adipisci cumque eveniet tempore?
                 </p>
